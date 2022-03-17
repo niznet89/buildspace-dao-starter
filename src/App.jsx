234c5619f47d13777b7a903c15710521c19ee432
Tenzin Rose <tenzinrose@gmail.com>
2022-03-17 16:45:54 +0000

    Checking whether the ENV file gets pushed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useMemo, useState } from "react";
+
+// import thirdweb
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  // Use the connectWallet hook thirdweb gives us.
+  const { connectWallet, address, error, provider} = useWeb3();
+  console.log("👋 Wallet", connectWallet)
+  console.log("👋 Address:", address)
+  console.log("👋 error:", error)
+  console.log("👋 provider:", provider)
+
+  // This is the case where the user hasn't connected their wallet
+  // to your web app. Let them call connectWallet.
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>SurfnWorkDAO</h1>
+          <p>The community for nomads/entreprenuers that love to shred</p>
+          <button onClick={() => connectWallet("injected")} className="btn-primary">
+            Connect your wallet</button>
+      </div>
+    );
+  }
+  // This is the case where we have the user's address
+  // which means they've connected their wallet to our site!
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
-    </div>
-  );
-};
-
+      <h1>👀 wallet connected, now what!</h1>
+    </div>);
+}
 export default App;
